864d2a8564bd56f3fdd8a58564c0c61772c9a800
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-07-13 12:44:44 +0100

    add Validator interfaces

---
+++ Interfaces/src/Validator/ValidatorInterface.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Cobra\Interfaces\Validator;
+
+/**
+ * Validator Interface
+ *
+ * @category  Validator
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+interface ValidatorInterface
+{
+    /**
+     * Returns the validator name.
+     *
+     * @return string
+     */
+    public function getName(): string;
+
+    /**
+     * Performs the validation and returns whether it has been successful.
+     *
+     * @param  mixed $value
+     * @return boolean
+     */
+    public function validate($value = null): bool;
+
+    /**
+     * Sets the validator error message.
+     *
+     * @param  string $message
+     * @return ValidatorInterface
+     */
+    public function setMessage(string $message): ValidatorInterface;
+
+    /**
+     * Returns the validator error message.
+     *
+     * @return string
+     */
+    public function getMessage(): string;
+}
---
+++ Interfaces/src/Validator/ValidatorResolverInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Cobra\Interfaces\Validator;
+
+/**
+ * Validator Resolver Interface
+ *
+ * @category  Validator
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+interface ValidatorResolverInterface
+{
+    /**
+     * Returns a validator instance based on a string identifier.
+     *
+     * @param string $value
+     * @return ValidatorInterface
+     * @throws InvalidClassnameException
+     */
+    public function get(string $value): ValidatorInterface;
+}
