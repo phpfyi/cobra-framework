1955d5c80b36f4f1efd17974b005931af564565a
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-07-16 22:24:52 +0100

    add MailService class

---
+++ Mail/src/Service/MailService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Cobra\Mail\Service;
+
+use Cobra\Core\Service\Service;
+
+/**
+ * Mail Service
+ *
+ * @category  Mail
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+class MailService extends Service
+{
+    /**
+     * Bind namespace references to classes in the container.
+     *
+     * @return void
+     */
+    public function namespaces(): void
+    {
+        contain_namespace(
+            \Cobra\Interfaces\Mail\MailerInterface::class,
+            \Cobra\Mail\Mailer::class
+        );
+        contain_namespace(
+            \Cobra\Interfaces\Mail\MailerConfigInterface::class,
+            \Cobra\Mail\MailerConfig::class
+        );
+        contain_namespace(
+            \Cobra\Interfaces\Mail\Smtp\SmtpMailerInterface::class,
+            \Cobra\Mail\Smtp\SmtpMailer::class
+        );
+        contain_namespace(
+            \Cobra\Interfaces\Mail\Smtp\SmtpMailerConfigInterface::class,
+            \Cobra\Mail\Smtp\SmtpMailerConfig::class
+        );
+    }
+}
