85294769f45ecdfae19fe93fa99ff90d72abe817
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-07-17 22:45:19 +0100

    add comment to Security functions

--- Security/_functions.php
+++ Security/_functions.php
@@ -3,6 +3,19 @@
 use Cobra\Interfaces\Security\Token\CsrfTokenInterface;
 use Cobra\Interfaces\Security\Token\NonceTokenInterface;
 
+/**
+ * Security function sets
+ *
+ * @category  Security
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+
 if (! function_exists('nonce')) {
     /**
      * Returns the nonce token
