2e279abbf4c4cc567713c6e153cc7e5c64902d61
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-07-18 00:32:22 +0100

    add Container Resolvable trait

---
+++ Container/src/Traits/Resolvable.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Cobra\Container\Traits;
+
+use Cobra\Container\Container;
+
+/**
+ * Resolvable Trait
+ *
+ * @category  Container
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+trait Resolvable
+{
+    /**
+     * Returns a resolved instance of the calling class.
+     *
+     * @param  mixed[] ...$args
+     * @return void
+     */
+    public static function resolve(...$args)
+    {
+        return Container::instance()->resolve(static::class, func_get_args($args));
+    }
+}
