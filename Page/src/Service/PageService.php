ece30dab2ac5691a3e39a6705059482a4d7628d1
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-07-21 18:36:21 +0100

    add PageService class

---
+++ Page/src/Service/PageService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Cobra\Page\Service;
+
+use Cobra\Core\Service\Service;
+
+/**
+ * Page Service
+ *
+ * @category  Page
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+class PageService extends Service
+{
+    /**
+     * Bind namespace references to classes in the container.
+     *
+     * @return void
+     */
+    public function namespaces(): void
+    {
+        contain_namespace(
+            \Cobra\Interfaces\Page\PageInterface::class,
+            \Cobra\Page\Page::class
+        );
+    }
+}
