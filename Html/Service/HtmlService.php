c84965099ec2e2cb88eb3fbfa7496da57ab14885
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-07-25 23:44:02 +0100

    add HtmlService class

---
+++ Html/Service/HtmlService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Cobra\Html\Service;
+
+use Cobra\Core\Service\Service;
+
+/**
+ * HTML Service
+ *
+ * @category  HTML
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+class HtmlService extends Service
+{
+    /**
+     * Bind namespace references to classes in the container.
+     *
+     * @return void
+     */
+    public function namespaces(): void
+    {
+        contain_namespace(
+            \Cobra\Interfaces\Html\HtmlElementInterface::class,
+            \Cobra\Html\HtmlElement::class
+        );
+    }
+}
