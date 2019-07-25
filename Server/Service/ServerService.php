faf6285e00f15bbe7844de9f84dd1b723602fe6b
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-07-25 23:21:09 +0100

    add ServerService class

---
+++ Server/Service/ServerService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Cobra\Server\Service;
+
+use Cobra\Core\Service\Service;
+
+/**
+ * Server Service
+ *
+ * @category  Server
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+class ServerService extends Service
+{
+    /**
+     * Bind namespace references to classes in the container.
+     *
+     * @return void
+     */
+    public function namespaces(): void
+    {
+        contain_namespace(
+            \Cobra\Interfaces\Server\File\FilePathInterface::class,
+            \Cobra\Server\File\FilePath::class
+        );
+    }
+}
