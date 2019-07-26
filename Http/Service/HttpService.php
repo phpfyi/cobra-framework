4cffbc7ec49c925e385ea86aa733780922d0843a
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-07-26 21:17:20 +0100

    merge all HTTP services

--- Http/Service/HttpService.php
+++ Http/Service/HttpService.php
@@ -3,6 +3,7 @@
 namespace Cobra\Http\Service;
 
 use Cobra\Core\Service\Service;
+use Cobra\Interfaces\Http\Uri\RequestUriInterface;
 
 /**
  * HTTP Service
@@ -29,5 +30,56 @@ class HttpService extends Service
             \Cobra\Interfaces\Http\Middleware\MiddlewareHandlerInterface::class,
             \Cobra\Http\Middleware\MiddlewareHandler::class
         );
+    }
+    /**
+     * Set up any service class instances required by the application.
+     *
+     * @return void
+     */
+    public function instances(): void
+    {
+        contain_object(
+            \Cobra\Interfaces\Http\Uri\RequestUriInterface::class,
+            $this->app->getRequest()->getUri()
+        );
+        contain_object(
+            \Cobra\Interfaces\Http\Message\RequestInterface::class,
+            $this->app->getRequest()
+        );
+        contain_object(
+            \Cobra\Interfaces\Http\Message\ResponseInterface::class,
+            $this->app->getResponse()
+        );
+
+        $uri = $this->app
+            ->getRequest()
+            ->getUri();
+            
+        $base = $uri
+            ->withPath('')
+            ->withQuery('');
+
+        $this
+            ->define('URL', $base->withPath($uri->getPath()))
+            ->define('URL_PATH', $uri->getPath() == '/' ? '' : $uri->getPath())
+            ->define('BASE_URL', $base)
+            ->define('BASE_URL_SLASH', $base->withPath('/'))
+            ->define('CSS', $base->withPath('/css/'))
+            ->define('IMG', $base->withPath('/img/'))
+            ->define('JS', $base->withPath('/js/'));
+    }
+
+    /**
+     * Defines a new constant with an escaped value suitable for output
+     * throughout the application
+     *
+     * @param  string $name
+     * @param  string $value
+     * @return self
+     */
+    protected function define(string $name, string $value): self
+    {
+        define($name, htmlspecialchars($value));
+        return $this;
     }
 }
