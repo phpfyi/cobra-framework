6cf06e1fa155888eea31159e24fbebe02b58a999
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-07-27 14:44:04 +0100

    add ViewObject interface

---
+++ Interfaces/View/ViewObject.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Cobra\Interfaces\View;
+
+/**
+ * View interface
+ *
+ * @category  View
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+interface ViewObject
+{
+    /**
+     * Returns an array of view data
+     *
+     * @return array
+     */
+    public function getViewData(): array;
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function getHTML();
+    /**
+     * Returns the rendered HTML template
+     *
+     * @return string
+     */
+    public function __toString(): string;
+}
