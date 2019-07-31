f97a845685f39d1d2d1561479940e1847915168e
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-07-31 18:43:26 +0100

    add AjaxRouteGuard class

---
+++ Routing/Guard/AjaxRouteGuard.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Cobra\Route\Guard;
+
+use Cobra\Routing\Guard\RouteGuard;
+
+/**
+ * AJAX Route Guard
+ *
+ * @category  Routing
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+class AjaxRouteGuard extends RouteGuard
+{
+    /**
+     * Returns whether the current route can be activated
+     *
+     * @return boolean
+     */
+    public function canActivate(): bool
+    {
+        if (!$this->request->isAjax()) {
+            $this->response = http_forbidden_response($this);
+            return false;
+        }
+        return true;
+    }
+}
