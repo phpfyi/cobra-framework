0246a801d6d031c13bab3911f1edc0518ef04652
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-08-03 02:33:51 +0100

    add DatabaseFactory class

---
+++ Database/Factory/DatabaseFactory.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Cobra\Database\Factory;
+
+use Cobra\Database\DatabaseTable;
+use Cobra\Database\Statement\ShowTablesStatement;
+use Cobra\Object\AbstractObject;
+
+/**
+ * Database Factory
+ *
+ * @category  Database
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+class DatabaseFactory extends AbstractObject
+{
+    /**
+     * Array of database tables to create / update
+     *
+     * @var array
+     */
+    protected $table = [];
+
+    /**
+     * Array of existing database tables
+     *
+     * @var array
+     */
+    protected $existingTables = [];
+
+    /**
+     * Sets the required properties.
+     *
+     * @param array $tables
+     * @param ShowTablesStatement $statment
+     */
+    public function __construct(array $tables, ShowTablesStatement $statment)
+    {
+        $this->tables = $tables;
+        $this->existingTables = $statment->run();
+    }
+
+    /**
+     * Creates the database.
+     *
+     * @return void
+     */
+    public function createDatabase(): void
+    {
+        $this->createTables($this->tables);
+    }
+
+    /**
+     * Creates a group of database tables.
+     *
+     * @param array $tables
+     * @return void
+     */
+    protected function createTables(array $tables): void
+    {
+        array_map(function (DatabaseTable $table) {
+            $this->createTable($table);
+            $this->createTables($table->getManyManyRelations());
+        }, $tables);
+    }
+
+    /**
+     * Creates a database table.
+     *
+     * @param DatabaseTable $table
+     * @return void
+     */
+    protected function createTable(DatabaseTable $table): void
+    {
+        $factory = container_resolve(
+            DatabaseTableFactory::class,
+            [
+                $table,
+                in_array($table->getTable(), $this->existingTables)
+            ]
+        );
+        $factory->migrate();
+    }
+}
