c1ef9a32ab94edbdc3d0568878295ed719320ac8
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-08-03 02:24:55 +0100

    add AlterTableAddColumnStatement and AlterTableChangeColumnStatement classes

---
+++ Database/Statement/AlterTableAddColumnStatement.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Cobra\Database\Statement;
+
+use Cobra\Database\Factory\DatabaseColumnFactory;
+use Cobra\Interfaces\Database\Field\DatabaseFieldInterface;
+use Cobra\Object\AbstractObject;
+
+/**
+ * Alter Table Add Column Statement
+ *
+ * @category  Database
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+class AlterTableAddColumnStatement extends AbstractObject
+{
+    /**
+     * The databse table name.
+     *
+     * @var string
+     */
+    protected $table;
+
+    /**
+     * DatabaseFieldInterface instance
+     *
+     * @var DatabaseFieldInterface
+     */
+    protected $column;
+
+    /**
+     * Sets the required properties.
+     *
+     * @param string $table
+     * @param string $column
+     */
+    public function __construct(string $table, DatabaseFieldInterface $column)
+    {
+        $this->table = $table;
+        $this->column = $column;
+    }
+
+    /**
+     * Executes the database query statement and returns the result.
+     *
+     * @return array
+     */
+    public function run(): bool
+    {
+        return stmt(sprintf(
+            "ALTER TABLE `%s` ADD %s",
+            $this->table,
+            container_resolve(DatabaseColumnFactory::class, [$this->column])->getSQL()
+        ))->execute();
+    }
+}
