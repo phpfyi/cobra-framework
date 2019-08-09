d3d977edb00c6a45ca350947863f2e9a294e6fc5
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-08-09 01:48:12 +0100

    add Model InsertAction class

---
+++ Model/Action/InsertAction.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Cobra\Model\Action;
+
+use Cobra\Database\Statement\InsertStatement;
+
+/**
+ * Insert Action
+ *
+ * @category  Model
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+class InsertAction extends Action
+{
+    /**
+     * Method hook before process
+     *
+     * @var string
+     */
+    protected $methodBefore = 'beforeSave';
+
+    /**
+     * Method hook after process
+     *
+     * @var string
+     */
+    protected $methodAfter = 'afterSave';
+
+    /**
+     * Array of changed columns for the class.
+     *
+     * @var array
+     */
+    protected $columns = [];
+
+    /**
+     * Processes a model hierarchy namespace.
+     *
+     * @param string $namespace
+     * @return void
+     */
+    protected function processNamespace(string $namespace): void
+    {
+        $this->columns = $this->model->extractFromClass($namespace, false);
+
+        if ($this->id) {
+            $this->columns['id'] = $this->id;
+        }
+        $this->statement = container_resolve(
+            InsertStatement::class,
+            [
+                schema($namespace)->get('table'),
+                $this->columns
+            ]
+        );
+        $this->id = $this->statement->run();
+    }
+}
