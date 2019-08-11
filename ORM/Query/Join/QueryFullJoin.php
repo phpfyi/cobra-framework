ce11ed91dcff76e37f1f9c6a7566ba485606bc78
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-08-11 19:43:41 +0100

    add QueryJoin sub classes

---
+++ ORM/Query/Join/QueryFullJoin.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Cobra\ORM\Query\Join;
+
+use Cobra\ORM\Query\QueryJoin;
+
+/**
+ * Query Full Join
+ *
+ * @category  ORM
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+
+class QueryFullJoin extends QueryJoin
+{
+    /**
+     * Join type
+     *
+     * @var string
+     */
+    protected $join = 'FULL JOIN';
+}
