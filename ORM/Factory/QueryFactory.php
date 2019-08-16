ddc19f01469ac24aa220a6fe779e9973079e2c29
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-08-16 23:33:45 +0100

    add QueryFactory class

---
+++ ORM/Factory/QueryFactory.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Cobra\ORM\Factory;
+
+use Cobra\Object\AbstractObject;
+use Cobra\ORM\Query\DeleteQuery;
+use Cobra\ORM\Query\InsertQuery;
+use Cobra\ORM\Query\SelectQuery;
+use Cobra\ORM\Query\UpdateQuery;
+
+/**
+ * Query Factory
+ *
+ * @category  ORM
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+
+class QueryFactory extends AbstractObject
+{
+    /**
+     * Returns a select statement object
+     *
+     * @param [string] ...$args
+     * @return SelectQuery
+     */
+    public function select(...$args): SelectQuery
+    {
+        return container_resolve(SelectQuery::class, $args);
+    }
+
+    /**
+     * Returns a insert statement object
+     *
+     * @param [string] ...$args
+     * @return InsertQuery
+     */
+    public function insert(...$args): InsertQuery
+    {
+        return container_resolve(InsertQuery::class, $args);
+    }
+
+    /**
+     * Returns a update statement object
+     *
+     * @param [string] ...$args
+     * @return UpdateQuery
+     */
+    public function update(...$args): UpdateQuery
+    {
+        return container_resolve(UpdateQuery::class, $args);
+    }
+
+    /**
+     * Returns a delete statement object
+     *
+     * @param [string] ...$args
+     * @return DeleteQuery
+     */
+    public function delete(...$args): DeleteQuery
+    {
+        return container_resolve(DeleteQuery::class, $args);
+    }
+}
