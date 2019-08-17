b32d912c70bfd82688e81afaa3e70f88eb9fedda
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-08-18 00:02:29 +0100

    add InvalidModelRelationException

---
+++ Model/Exception/InvalidModelRelationException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Cobra\Model\Exception;
+
+/**
+ * Invalid Model Relation Exception
+ *
+ * @category  Model
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+class InvalidModelRelationException extends \Exception
+{
+    
+}
