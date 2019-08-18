3641f7bf79cd944fd8b9a6e4fc4a85fdde520553
Andrew Mc Cormack <webmaster@ddmseo.com>
2019-08-19 00:34:45 +0100

    add UsesQueryIdentifier trait

---
+++ Database/Query/Traits/UsesQueryIdentifier.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Cobra\Database\Query\Traits;
+
+/**
+ * Uses Query Identifier trait
+ *
+ * @category  ORM
+ * @package   Cobra
+ * @author    Andrew Mc Cormack <webmaster@ddmseo.com>
+ * @copyright Copyright (c) 2019, Andrew Mc Cormack
+ * @license   https://github.com/phpfyi/cobra-framework/issues
+ * @version   1.0.0
+ * @link      https://github.com/phpfyi/cobra-framework
+ * @since     1.0.0
+ */
+
+trait UsesQueryIdentifier
+{
+    /**
+     * Query object ID
+     *
+     * @var string
+     */
+    protected $qid;
+
+    /**
+     * Sets the query identifier.
+     *
+     * @return void
+     */
+    protected function setQID(): void
+    {
+        $this->qid = base64_encode(random_bytes(10));
+    }
+}
